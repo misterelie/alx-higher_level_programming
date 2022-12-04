acc6e46a9e8deea1fcb0cbca8344f7f931713383
misterelie <70035889+misterelie@users.noreply.github.com>
2022-12-04 02:40:50 +0000

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,12 +5,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-typedef struct listin_s
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
-	int i;
+	int n;
 	struct listint_s *next;
-
-}listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
