274d0bc5b7356bc5e8a51ac9d3e354b07c6c0828
misterelie <70035889+misterelie@users.noreply.github.com>
2022-12-14 09:52:39 +0000

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+    new_d = {}
+    for i in a_dictionary:
+        new_d[i] = a_dictionary[i] * 2
+    return new_d
